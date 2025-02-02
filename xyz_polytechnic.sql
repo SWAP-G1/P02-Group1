72089badcd35c68a83b1f90340eb89b1f71bb693
homzomtp <2302422g@student.tp.edu.sg>
2025-02-03 01:01:16 +0800

    update sql

--- xyz_polytechnic.sql
+++ xyz_polytechnic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2025 at 05:48 PM
+-- Generation Time: Feb 02, 2025 at 06:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
